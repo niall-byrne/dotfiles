bfb46a2055591cb4b89016e8098000c3cd42dd7c
Niall Byrne <niall@sharedvisionsolutions.com>
2018-07-22 17:19:35 -0400

    Install Automation

---
+++ home/bin/___osx-bootstrap.sh
@@ -0,0 +1,27 @@
+# Preparing for install automation.
+
+xcode-select --install
+brew install brew-cask
+brew cask install google-chrome
+brew cask install pycharm-ce
+brew cask install marshallofsound-google-play-music-player
+brew cask install synergy
+brew cask install windscribe
+brew cask install keepassxc
+brew cask install skype
+brew cask install docker
+brew cask install tunnelblock
+brew cask install skype
+brew cask install virtualbox
+brew cask install textmate
+brew cask install vagrant
+brew cask install iterm2
+brew cask install keybase
+
+#neoffice
+#little snitch
+#oversite
+#
+#dotfiles
+#yubikey
+
